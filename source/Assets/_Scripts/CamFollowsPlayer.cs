68cf5ff0ff2c99b76fb89376ae596887bc3377d4
Alucard1766 <luca.salzani@gmail.com>
2015-01-29 18:01:38 +0100

    Camera follows player1 now (+Deleted old files)
    Closes #1
    - Smoothly
    - A little bit in front of the player
    - Ready to implement for 2 players

---
+++ source/Assets/_Scripts/CamFollowsPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollowsPlayer : MonoBehaviour {
+
+
+
+	public Transform target;
+	public float smooth = 5.0f;
+	public float divisorYAxisOffset = 2f;
+
+	//TODO: Implement Start() with starting point betweent both starts
+
+	void Update () {
+		Vector3 desiredCameraPosition = target.position;
+		desiredCameraPosition.z = transform.position.z; //Lock Z-Axis
+		desiredCameraPosition.y += Camera.main.orthographicSize/divisorYAxisOffset;
+		transform.position = Vector3.Lerp(transform.position, desiredCameraPosition, Time.deltaTime * smooth);
+		                               
+	}
+}
