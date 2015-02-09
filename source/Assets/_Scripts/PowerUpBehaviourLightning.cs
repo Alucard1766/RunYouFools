2458501386251c596aea9ba0afbb54dd71e49baa
Alucard1766 <luca.salzani@gmail.com>
2015-02-09 20:29:34 +0100

    Made Beer preFab, started work at Lightning, ref #10

---
+++ source/Assets/_Scripts/PowerUpBehaviourLightning.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpBehaviourLightning : PowerUpBehaviour {
+	
+	
+	// Use this for initialization
+	protected override void Start () {
+		powerUpType = "Sword";
+		base.Start ();
+	}
+
+
+	protected override void Update(){
+		Object.Destroy (gameObject);
+	}
+	
+	
+	public override void FirePowerUp(GameObject affectedPlayer){
+		//TODO: Implement
+	}
+}
